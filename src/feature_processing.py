c3da2511f6b13c1691c5563617a05f958a028ccb
ayyesu <iamdanielkissiedu@gmail.com>
2024-01-17 22:29:00 +0000

    clean text column with stopwords, links, special symbols and language errors

--- src/feature_processing.py
+++ src/feature_processing.py
@@ -2,10 +2,40 @@ import pandas as pd
 import numpy as np
 from src import config
 
+# Natural Language toolkit
+import nltk
+nltk.download('stopwords')
+stemmer = nltk.SnowballStemmer("english")
+from nltk.corpus import stopwords
+import string
+stopword=set(stopwords.words('english'))
 
+# Regular Expression
+import re
+
+df = pd.read_csv(config.DATASET)
 def feature_processing(data: pd.DataFrame):
-    df = pd.read_csv(data)
+
     print(df.head())
 
+    # Checking for null values
+    print(df.isnull().sum())
 
-feature_processing(config.DATASET)
+
+
+feature_processing(df)
+
+def clean(text):
+    text = str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    text = [word for word in text.split(' ') if word not in stopword]
+    text=" ".join(text)
+    text = [stemmer.stem(word) for word in text.split(' ')]
+    text=" ".join(text)
+    return text
+df["text"] = df["text"].apply(clean)
